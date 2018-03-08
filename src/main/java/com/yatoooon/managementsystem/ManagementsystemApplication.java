cf1133daddccebb1e80c72e22286d7b95e19b444
yatoooon <137413684@qq.com>
2018-03-08 19:58:58 +0800

    first commit

---
+++ src/main/java/com/yatoooon/managementsystem/ManagementsystemApplication.java
@@ -0,0 +1,12 @@
+package com.yatoooon.managementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementsystemApplication.class, args);
+	}
+}
